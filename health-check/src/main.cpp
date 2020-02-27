6d6b3523890423f27e57bac465f10e2c9672c75b
Tim Bolender <git@timbolender.de>
2020-02-28 00:15:54 +0100

    Set monitor speed

--- health-check/src/main.cpp
+++ health-check/src/main.cpp
@@ -8,8 +8,7 @@ bool error = true;
 bool errorShow = false;
 
 void setup() {
-    Wire.begin(D0, D5);
-    Serial.begin(115200);
+    Serial.begin(74880);
 
     // Sanity check delay - allows reprogramming if accidently blowing power w/leds
     delay(3000);
