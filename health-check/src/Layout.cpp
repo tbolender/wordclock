2ed6c29d0d30f8d9984d0e16ddc8e01007ed7b40
Tim Bolender <git@timbolender.de>
2020-02-29 13:07:44 +0100

    Fix non-used characters in array

--- health-check/src/Layout.cpp
+++ health-check/src/Layout.cpp
@@ -9,33 +9,33 @@ const int Layout::IT_IS[] = {
 };
 
 const int Layout::MINUTES[12][12] = {
-    { 9, 10, 11 }, // UHR
-    { 103, 102, 101, 100, 74, 75, 76, 77 }, // FÜNF NACH
-    { 89, 90, 91, 92, 74, 75, 76, 77 }, // ZEHN NACH
-    { 84, 83, 82, 81, 80, 79, 78, 74, 75, 76, 77 }, // VIERTEL NACH
-    { 93, 94, 95, 96, 97, 98, 99, 74, 75, 76, 77 }, // ZWANZIG NACH
-    { 103, 102, 101, 100, 67, 68, 69, 66, 65, 64, 63 }, // FÜNF VOR HALB
-    { 66, 65, 64, 63 }, // HALB
+    {   9,  10,  11,  -1, -1, -1, -1, -1, -1, -1, -1, -1 }, // UHR
+    { 103, 102, 101, 100, 74, 75, 76, 77, -1, -1, -1, -1 }, // FÜNF NACH
+    {  89,  90,  91,  92, 74, 75, 76, 77, -1, -1, -1, -1 }, // ZEHN NACH
+    {  84,  83,  82,  81, 80, 79, 78, 74, 75, 76, 77, -1 }, // VIERTEL NACH
+    {  93,  94,  95,  96, 97, 98, 99, 74, 75, 76, 77, -1 }, // ZWANZIG NACH
+    { 103, 102, 101, 100, 67, 68, 69, 66, 65, 64, 63, -1 }, // FÜNF VOR HALB
+    {  66,  65,  64,  63, -1, -1, -1, -1, -1, -1, -1, -1 }, // HALB
     { 103, 102, 101, 100, 74, 75, 76, 77, 66, 65, 64, 63 }, // FÜNF NACH HALB
-    { 93, 94, 95, 96, 97, 98, 99, 67, 68, 69 }, // ZWANZIG VOR
-    { 84, 83, 82, 81, 80, 79, 78, 67, 68, 69 }, // VIERTEL VOR
-    { 89, 90, 91, 92, 67, 68, 69 }, // ZEHN VOR
-    { 103, 102, 101, 100, 67, 68, 69 }, // FÜNF VOR
+    {  93,  94,  95,  96, 97, 98, 99, 67, 68, 69, -1, -1 }, // ZWANZIG VOR
+    {  84,  83,  82,  81, 80, 79, 78, 67, 68, 69, -1, -1 }, // VIERTEL VOR
+    {  89,  90,  91,  92, 67, 68, 69, -1, -1, -1, -1, -1 }, // ZEHN VOR
+    { 103, 102, 101, 100, 67, 68, 69, -1, -1, -1, -1, -1 }, // FÜNF VOR
 };
 
 const int Layout::HOURS[12][6] = {
-    { 16, 15, 14, 13, 12 }, // ZWÖLF
-    { 45, 46, 47, 48 }, // EINS
-    { 52, 53, 54, 55 }, // ZWEI
-    { 44, 43, 42, 41 }, // DREI
-    { 37, 36, 35, 34 }, // VIER
-    { 59, 58, 57, 56 }, // FÜNF
-    { 23, 24, 25, 26, 27 }, // SECHS
+    { 16, 15, 14, 13, 12, -1 }, // ZWÖLF
+    { 45, 46, 47, 48, -1, -1 }, // EINS
+    { 52, 53, 54, 55, -1, -1 }, // ZWEI
+    { 44, 43, 42, 41, -1, -1 }, // DREI
+    { 37, 36, 35, 34, -1, -1 }, // VIER
+    { 59, 58, 57, 56, -1, -1 }, // FÜNF
+    { 23, 24, 25, 26, 27, -1 }, // SECHS
     { 22, 21, 20, 19, 18, 17 }, // SIEBEN
-    { 30, 31, 32, 33 }, // ACHT
-    { 4, 5, 6, 7 }, // NEUN
-    { 1, 2, 3, 4 }, // ZEHN
-    { 57, 58, 59 }, // ELF
+    { 30, 31, 32, 33, -1, -1 }, // ACHT
+    {  4,  5,  6,  7, -1, -1 }, // NEUN
+    {  1,  2,  3,  4, -1, -1 }, // ZEHN
+    { 61, 60, 59, -1, -1, -1 }, // ELF
 };
 
 Layout::Layout() {
@@ -71,6 +71,7 @@ void Layout::setMinutes(int minutes) {
     minutes /= 5;
 
     for(int i : MINUTES[minutes]) {
+        if(i < 0) break;
         ledMask[i] = true;
     }
 }
@@ -82,13 +83,14 @@ void Layout::setHours(int hours, int minutes) {
 
     // FIXME: Handle EIN UHR case
     for(int i : HOURS[hours]) {
+        if(i < 0) break;
         ledMask[i] = true;
     }
 }
 
 bool* Layout::getLayout(int hours, int minutes) {
     clear();
-    // TODO: Figure out what happens to non-initialized array elements
+
     setItIs();
     setSingleMinutes(minutes);
     setMinutes(hours);
